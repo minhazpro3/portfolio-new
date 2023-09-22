4901a4d2759cfb638331eb074bd4f3dfb55f2865
minhazpro3 <minhazpro1@gmail.com>
2023-09-22 23:55:15 +0600

    fix bug of deploy

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import minhaz from "../../public/images/minhaz.jpg";
 const About = () => {
+  const im = "I'm"
   return (
     <div className="container mx-auto px-8 md:px-0 mb-8">
       <div className="">
@@ -16,7 +17,7 @@ const About = () => {
             />
           </div>
           <div className="z-10  bg-gradient-to-tr from-[#100e29] via-[#152b44] to-[#0F172A] relative md:absolute lg:pl-14 md:pl-8 md:pr-16 lg:pr-20 lg:py-12 py-6 px-4 sm:px-0 sm:pl-5 sm:pr-12 my-10 md:my-0 md:top-6 lg:top-20 md:left-[40%] sm:rounded-tr-full rounded-tr-[6rem] sm:rounded-none">
-            <h2 className="xl:text-3xl lg:text-2xl md:text-[16px sm:text-2xl text-xl text-[#8ff577] font-bold">Hi! I'm Md Minhaz</h2>
+            <h2 className="xl:text-3xl lg:text-2xl md:text-[16px sm:text-2xl text-xl text-[#8ff577] font-bold">Hi! {im} Md Minhaz</h2>
             <p className="xl:text-base lg:text-base md:text-[14px] sm:text-xl text-sm mb-2 text-[#8e8affc2]">
               Front End Developer, Content Creator
             </p>
