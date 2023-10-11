2a6419381ae4f6668333f0de16e74d7249ae297d
minhazpro3 <minhazpro1@gmail.com>
2023-10-11 22:06:55 +0600

    my project skill set

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import React from "react";
+import {AiFillEye} from "react-icons/ai"
+import {BiCodeAlt} from "react-icons/bi"
 
 const Projects = () => {
   return (
@@ -17,9 +19,9 @@ const Projects = () => {
               </p>
               <h5 className="text-xl font-bold text-sky-400 my-4 opacity-80">Skills</h5>
               <p className="text-white bg-gradient-to-r from-[#4239c2] to-[#100e29] inline-block px-4 py-1 rounded-full">React </p>
-              <div>
-                <button>View Code</button>
-                <button>Live Demo</button>
+              <div className="flex gap-x-4 my-6">
+                <button className="flex items-center gap-x-2 border-2 border-[#4239c2] text-white px-4 py-1 rounded-md bg-gradient-to-r from-[#3629e9d0] to-[#25233c] " > <AiFillEye/> View Code</button>
+                <button className="flex items-center gap-x-2 border-2 border-[#4239c2] text-white px-4 py-1 rounded-md bg-gradient-to-r from-[#3629e9d0] to-[#25233c]" > <BiCodeAlt/> Live Demo</button>
               </div>
             </div>
             <div className="relative w-full h-[300px] sm:h-[450px] md:h-[270px] lg:h-[300px]">
